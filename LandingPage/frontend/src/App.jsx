f6c4711d0dd6ec65f5d0091fcf953909880a6f0d
Mayura Thayalan <thayalanmayura@gmail.com>
2025-11-15 10:40:58 +0530

    completed the footer

--- LandingPage/frontend/src/App.jsx
+++ LandingPage/frontend/src/App.jsx
@@ -1,11 +1,27 @@
-import React from 'react'
-import GetInToch from "../src/components/GetInToch.jsx"
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Footer from "../src/components/Footer.jsx";
+import Home from "../src/pages/Home.jsx";
+import Features from "../src/pages/Features.jsx";
+import About from "../src/pages/About.jsx";
+import Contact from "../src/pages/Contact.jsx";
+import Privacy from "../src/pages/Privacy.jsx";
 const App = () => {
   return (
     <div>
-      <GetInToch/>
-    </div>
-  )
-}
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/features" element={<Features />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/privacy" element={<Privacy />} />
+        </Routes>
 
-export default App
+        <Footer />
+      </BrowserRouter>
+    </div>
+  );
+};
+
+export default App;
---
+++ LandingPage/frontend/src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import GetInToch from "../components/GetInToch.jsx";
+
+const Home = () => {
+  return (
+    <div>
+      <GetInToch />
+    </div>
+  );
+};
+
+export default Home;
