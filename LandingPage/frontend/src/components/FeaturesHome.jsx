8e796d03bacabc3eaabb033b51e30f74a65fed20
Umesha Liyanage <umesha.liyanage.lk@gmail.com>
2025-11-16 19:12:29 +0530

    features-cards-initial

---
+++ LandingPage/frontend/src/components/FeaturesHome.jsx
@@ -0,0 +1,59 @@
+import React from "react";
+import Placement from '../assets/ar-placement.png'
+import Img2 from '../assets/img2.png'
+import Img3 from '../assets/img3.png'
+import Img4 from '../assets/img4.png'
+import Img5 from '../assets/img5.png'
+import CardStacker from '../components/CardStacker'
+
+const FeaturesHome = () => {
+
+  const data = [
+    {
+      title: 'Kirby',
+      subtitle: 'Star Allies',
+      rating: '4.7',
+      backgroundColors: { top: '#FBDA35', bottom: '#E3A237' },
+      image: Placement,
+    },
+    {
+      title: 'Mario',
+      subtitle: 'Super Bros',
+      rating: '4.8',
+      backgroundColors: { top: '#FBDA35', bottom: '#E3A237' },
+      image: Img2,
+    },
+    {
+      title: 'Pokemon',
+      subtitle: 'Bulbasaur',
+      rating: '4.9',
+      backgroundColors: { top: '#FBDA35', bottom: '#E3A237' },
+      image: Img3,
+    },
+    {
+      title: 'Sonic',
+      subtitle: 'Blue Sonic',
+      rating: '4.9',
+      backgroundColors: { top: '#FBDA35', bottom: '#E3A237' },
+      image: Img4,
+    },
+    {
+      title: 'Pokemon',
+      subtitle: 'Pikachu',
+      rating: '5.0',
+      backgroundColors: { top: '#FBDA35', bottom: '#E3A237' },
+      image: Img5,
+    },
+  ];
+
+  return (
+    <div className="w-screen h-screen overflow-hidden flex items-center justify-center bg-black">
+      
+     
+      <CardStacker data={data} />
+
+    </div>
+  );
+}
+
+export default FeaturesHome;
--- LandingPage/frontend/src/pages/Home.jsx
+++ LandingPage/frontend/src/pages/Home.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 import GetInToch from "../components/GetInToch.jsx";
+import FeaturesHome from "../components/FeaturesHome.jsx";
 
 const Home = () => {
   return (
     <div>
+      <FeaturesHome />
       <GetInToch />
+
     </div>
   );
 };
