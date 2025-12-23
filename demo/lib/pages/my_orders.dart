0f61e1ced9b71c3dc12a0ddeacbc88a2e3f5e7e2
MayuraThayalan <thayalanmayura@gmail.com>
2025-12-23 16:04:52 +0530

    made the content change when the order progress is selected from the nav bar

--- demo/lib/pages/my_orders.dart
+++ demo/lib/pages/my_orders.dart
@@ -18,6 +18,19 @@ class MyOrdersState extends State<MyOrders> {
   bool isClicked = false;
   String selectedBtn = "Delivered";
 
+  Widget orderProgress() {
+    switch (selectedBtn) {
+      case "Delivered":
+        return deliveredOrders();
+      case "Processing":
+        return inProgressOrders();
+      case "Cancelled":
+        return cancelledOrders();
+      default:
+        return SizedBox();
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -76,103 +89,286 @@ class MyOrdersState extends State<MyOrders> {
                 );
               }).toList(),
             ),
-
+            orderProgress(),
             SizedBox(height: 25),
-            Container(
-              child: Column(
-                children: [
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-
-                    children: [
-                      Text(
-                        "Order N.1947034",
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 25,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                      Text("05-12-2025", style: myOrderNormalStyle),
-                    ],
-                  ),
-                  SizedBox(height: 25),
-                  Padding(
-                    padding: const EdgeInsets.only(left: 8.0),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-
-                      children: [
-                        Row(
-                          children: [
-                            Text("Quantity: ", style: myOrderNormalStyle),
-                            Text(
-                              "3",
-                              style: TextStyle(
-                                color: Colors.white,
-                                fontSize: 25,
-                                fontWeight: FontWeight.bold,
-                              ),
-                            ),
-                          ],
-                        ),
-                        Row(
-                          children: [
-                            Text("Total Amount: ", style: myOrderNormalStyle),
-                            Text(
-                              "112",
-                              style: TextStyle(
-                                color: Colors.white,
-                                fontSize: 25,
-                                fontWeight: FontWeight.bold,
-                              ),
-                            ),
-                            Icon(
-                              Icons.attach_money,
-                              color: Colors.white,
-                              size: 25,
-                            ),
-                          ],
-                        ),
-                      ],
-                    ),
-                  ),
-                  SizedBox(height: 10),
-                  Padding(
-                    padding: const EdgeInsets.only(left: 10.0),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-
-                      children: [
-                        Container(
-                          padding: const EdgeInsets.symmetric(
-                            horizontal: 20,
-                            vertical: 4,
-                          ),
-                          decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(20),
-                            border: Border.all(color: Colors.white, width: 2),
-                          ),
-                          child: Text("Details ", style: myOrderNormalStyle),
-                        ),
-
-                        Text(
-                          "Delivered ",
-                          style: TextStyle(
-                            color: const Color.fromARGB(255, 123, 252, 128),
-                            fontSize: 20,
-
-                            fontWeight: FontWeight.w300,
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                ],
-              ),
-            ),
           ],
         ),
+      ),
+    );
+  }
+
+  Widget deliveredOrders() {
+    return Container(
+      child: Column(
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+
+            children: [
+              Text(
+                "Order N.1947034",
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 25,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Text("05-12-2025", style: myOrderNormalStyle),
+            ],
+          ),
+          SizedBox(height: 25),
+          Padding(
+            padding: const EdgeInsets.only(left: 8.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+
+              children: [
+                Row(
+                  children: [
+                    Text("Quantity: ", style: myOrderNormalStyle),
+                    Text(
+                      "3",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 25,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ],
+                ),
+                Row(
+                  children: [
+                    Text("Total Amount: ", style: myOrderNormalStyle),
+                    Text(
+                      "112",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 25,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    Icon(Icons.attach_money, color: Colors.white, size: 25),
+                  ],
+                ),
+              ],
+            ),
+          ),
+          SizedBox(height: 10),
+          Padding(
+            padding: const EdgeInsets.only(left: 10.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+
+              children: [
+                Container(
+                  padding: const EdgeInsets.symmetric(
+                    horizontal: 20,
+                    vertical: 4,
+                  ),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(20),
+                    border: Border.all(color: Colors.white, width: 2),
+                  ),
+                  child: Text("Details ", style: myOrderNormalStyle),
+                ),
+
+                Text(
+                  "Delivered ",
+                  style: TextStyle(
+                    color: const Color.fromARGB(255, 123, 252, 128),
+                    fontSize: 20,
+
+                    fontWeight: FontWeight.w300,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget inProgressOrders() {
+    return Container(
+      child: Column(
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+
+            children: [
+              Text(
+                "Order N.1947034",
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 25,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Text("05-12-2025", style: myOrderNormalStyle),
+            ],
+          ),
+          SizedBox(height: 25),
+          Padding(
+            padding: const EdgeInsets.only(left: 8.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+
+              children: [
+                Row(
+                  children: [
+                    Text("Quantity: ", style: myOrderNormalStyle),
+                    Text(
+                      "3",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 25,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ],
+                ),
+                Row(
+                  children: [
+                    Text("Total Amount: ", style: myOrderNormalStyle),
+                    Text(
+                      "112",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 25,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    Icon(Icons.attach_money, color: Colors.white, size: 25),
+                  ],
+                ),
+              ],
+            ),
+          ),
+          SizedBox(height: 10),
+          Padding(
+            padding: const EdgeInsets.only(left: 10.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+
+              children: [
+                Container(
+                  padding: const EdgeInsets.symmetric(
+                    horizontal: 20,
+                    vertical: 4,
+                  ),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(20),
+                    border: Border.all(color: Colors.white, width: 2),
+                  ),
+                  child: Text("Details ", style: myOrderNormalStyle),
+                ),
+
+                Text(
+                  "In Progress ",
+                  style: TextStyle(
+                    color: const Color.fromARGB(255, 123, 252, 128),
+                    fontSize: 20,
+
+                    fontWeight: FontWeight.w300,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget cancelledOrders() {
+    return Container(
+      child: Column(
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+
+            children: [
+              Text(
+                "Order N.1947034",
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 25,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Text("05-12-2025", style: myOrderNormalStyle),
+            ],
+          ),
+          SizedBox(height: 25),
+          Padding(
+            padding: const EdgeInsets.only(left: 8.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+
+              children: [
+                Row(
+                  children: [
+                    Text("Quantity: ", style: myOrderNormalStyle),
+                    Text(
+                      "3",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 25,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ],
+                ),
+                Row(
+                  children: [
+                    Text("Total Amount: ", style: myOrderNormalStyle),
+                    Text(
+                      "112",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 25,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    Icon(Icons.attach_money, color: Colors.white, size: 25),
+                  ],
+                ),
+              ],
+            ),
+          ),
+          SizedBox(height: 10),
+          Padding(
+            padding: const EdgeInsets.only(left: 10.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+
+              children: [
+                Container(
+                  padding: const EdgeInsets.symmetric(
+                    horizontal: 20,
+                    vertical: 4,
+                  ),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(20),
+                    border: Border.all(color: Colors.white, width: 2),
+                  ),
+                  child: Text("Details ", style: myOrderNormalStyle),
+                ),
+
+                Text(
+                  "Cancelled ",
+                  style: TextStyle(
+                    color: const Color.fromARGB(255, 123, 252, 128),
+                    fontSize: 20,
+
+                    fontWeight: FontWeight.w300,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
