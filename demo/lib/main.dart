45df36f536bcc15e6c5213c2d4344c8405b82d79
Umesha Liyanage <umesha.liyanage.lk@gmail.com>
2025-12-24 01:18:46 +0530

    homepage and bottom nav bar

--- demo/lib/main.dart
+++ demo/lib/main.dart
@@ -1,32 +1,19 @@
-import 'package:demo/pages/login.dart';
-import 'package:demo/providers/user_provider.dart';
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
-import 'package:provider/provider.dart';
+import 'homePage.dart';
 
 void main() {
-  runApp(
-    MultiProvider(
-      providers: [ChangeNotifierProvider(create: (_) => UserProvider())],
-      child: const MyApp(),
-    ),
-  );
+  runApp(MyApp());
 }
 
-
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return GetMaterialApp(
-      title: 'Lumeo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-      ),
-      home: Login(),
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
+      theme: ThemeData.dark().copyWith(
+        scaffoldBackgroundColor: Color(0xFF121212), // dark background
+      ),
+      home: HomePage(),
     );
   }
 }
