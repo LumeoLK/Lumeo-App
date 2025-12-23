e8b6afb18bfd42a2a6288bc3b9025800a3eb52ad
MayuraThayalan <thayalanmayura@gmail.com>
2025-12-23 12:56:23 +0530

    Compleated the frontend to display an order

--- demo/lib/pages/my_orders.dart
+++ demo/lib/pages/my_orders.dart
@@ -7,10 +7,11 @@ class MyOrders extends StatefulWidget {
   State<MyOrders> createState() => MyOrdersState();
 }
 
-final TextStyle myOrderNavStyle = const TextStyle(
+final TextStyle myOrderNormalStyle = const TextStyle(
   color: Colors.white,
   fontSize: 20,
-  fontWeight: FontWeight.bold,
+
+  fontWeight: FontWeight.w300,
 );
 
 class MyOrdersState extends State<MyOrders> {
@@ -37,10 +38,104 @@ class MyOrdersState extends State<MyOrders> {
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
 
               children: [
-                Text("Delivered", style: myOrderNavStyle),
-                Text("Processing", style: myOrderNavStyle),
-                Text("Cancelled", style: myOrderNavStyle),
+                Text("Delivered", style: myOrderNormalStyle),
+                Text("Processing", style: myOrderNormalStyle),
+                Text("Cancelled", style: myOrderNormalStyle),
               ],
+            ),
+            SizedBox(height: 25),
+            Container(
+              child: Column(
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+
+                    children: [
+                      Text(
+                        "Order N.1947034",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 25,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      Text("05-12-2025", style: myOrderNormalStyle),
+                    ],
+                  ),
+                  SizedBox(height: 25),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 8.0),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+
+                      children: [
+                        Row(
+                          children: [
+                            Text("Quantity: ", style: myOrderNormalStyle),
+                            Text(
+                              "3",
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 25,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ],
+                        ),
+                        Row(
+                          children: [
+                            Text("Total Amount: ", style: myOrderNormalStyle),
+                            Text(
+                              "112",
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 25,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            Icon(
+                              Icons.attach_money,
+                              color: Colors.white,
+                              size: 25,
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                  SizedBox(height: 10),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 10.0),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+
+                      children: [
+                        Container(
+                          padding: const EdgeInsets.symmetric(
+                            horizontal: 20,
+                            vertical: 4,
+                          ),
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(20),
+                            border: Border.all(color: Colors.white, width: 2),
+                          ),
+                          child: Text("Details ", style: myOrderNormalStyle),
+                        ),
+
+                        Text(
+                          "Delivered ",
+                          style: TextStyle(
+                            color: const Color.fromARGB(255, 123, 252, 128),
+                            fontSize: 20,
+
+                            fontWeight: FontWeight.w300,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
             ),
           ],
         ),
