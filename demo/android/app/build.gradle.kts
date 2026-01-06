ae0acf180b54c4f258f1edbdbf92c6793d7a33c0
MayuraThayalan <thayalanmayura@gmail.com>
2026-01-06 11:15:37 +0530

    made changes in the build gradels for google sign in

--- demo/android/app/build.gradle.kts
+++ demo/android/app/build.gradle.kts
@@ -2,7 +2,6 @@ plugins {
     id("com.android.application")
     id("kotlin-android")
     id("dev.flutter.flutter-gradle-plugin")
-    
 }
 
 android {
@@ -26,23 +25,35 @@ android {
         versionCode = flutter.versionCode
         versionName = flutter.versionName
     }
-signingConfigs {
-    create("release") {
-        storeFile = file("keystore/test.jks")
 
-        storePassword = System.getenv("KEYSTORE_PASSWORD")
-
-     
-        keyAlias = "androiddebugkey"
-
-    
-        keyPassword = System.getenv("KEYSTORE_PASSWORD")
+    signingConfigs {
+        // Shared debug keystore for all team members
+        getByName("debug") {
+            storeFile = file("keystore/debug.keystore")
+            storePassword = "android"
+            keyAlias = "androiddebugkey"
+            keyPassword = "android"
+        }
+        
+        // Release keystore
+        create("release") {
+            storeFile = file("keystore/test.jks")
+            storePassword = System.getenv("KEYSTORE_PASSWORD")
+            keyAlias = "androiddebugkey"
+            keyPassword = System.getenv("KEYSTORE_PASSWORD")
+        }
     }
-}
+
+    buildTypes {
+        getByName("debug") {
+            signingConfig = signingConfigs.getByName("debug")
+        }
+        getByName("release") {
+            signingConfig = signingConfigs.getByName("release")
+        }
+    }
 }
 
 flutter {
     source = "../.."
 }
-
-
