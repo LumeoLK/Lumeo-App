e983c646cb9f064f7cbc6967557ffa2480863366
Samindi Liyanage <162615413+ItsJustCharlie@users.noreply.github.com>
2026-02-01 18:07:04 +0530

    integrating login

--- demo/lib/services/auth_service.dart
+++ demo/lib/services/auth_service.dart
@@ -1,97 +1,97 @@
-// import 'package:demo/Constants.dart';
-// import 'package:demo/model/user.dart';
-// import 'package:demo/pages/homePage.dart';
-// import 'package:demo/pages/login.dart';
-// import 'package:demo/providers/user_provider.dart';
-// import 'package:demo/utils/utils.dart';
-// import 'package:flutter/material.dart';
-// import 'package:http/http.dart' as http;
-// import 'dart:convert';
-// import 'package:provider/provider.dart';
-// import 'package:shared_preferences/shared_preferences.dart';
+import 'package:demo/Constants.dart';
+import 'package:demo/model/user.dart';
+import 'package:demo/pages/home_page.dart';
+import 'package:demo/pages/login.dart';
+import 'package:demo/providers/user_provider.dart';
+import 'package:demo/utils/utils.dart';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+import 'package:provider/provider.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-// class AuthService {
-//   void signUpUser({
-//     required BuildContext context,
-//     required String email,
-//     required String name,
-//     required String password,
-//   }) async {
-//     try {
-//       // Create a map with the registration data including password
-//       final Map<String, dynamic> userData = {
-//         'name': name,
-//         'email': email,
-//         'password': password,
-//       };
+class AuthService {
+  void signUpUser({
+    required BuildContext context,
+    required String email,
+    required String name,
+    required String password,
+  }) async {
+    try {
+      // Create a map with the registration data including password
+      final Map<String, dynamic> userData = {
+        'name': name,
+        'email': email,
+        'password': password,
+      };
 
-//       http.Response res = await http.post(
-//         Uri.parse('${Constants.uri}/register'),
-//         body: jsonEncode(userData), // Send the actual data
-//         headers: <String, String>{
-//           'Content-Type': 'application/json; charset=UTF-8',
-//         },
-//       );
+      http.Response res = await http.post(
+        Uri.parse('${Constants.uri}/register'),
+        body: jsonEncode(userData), // Send the actual data
+        headers: <String, String>{
+          'Content-Type': 'application/json; charset=UTF-8',
+        },
+      );
 
-//       httpErrorHandle(
-//         response: res,
-//         context: context,
-//         onSuccess: () {
-//           showSnackBar(
-//             context,
-//             'Registered successfully!',
-//           );
-//           Navigator.of(context).pushAndRemoveUntil(
-//             MaterialPageRoute(builder: (context) => const Homepage()),
-//             (route) => false,
-//           );
-//         },
-//       );
-//     } catch (e) {
-//       showSnackBar(context, e.toString());
-//     }
-//   }
+      httpErrorHandle(
+        response: res,
+        context: context,
+        onSuccess: () {
+          showSnackBar(
+            context,
+            'Registered successfully!',
+          );
+          Navigator.of(context).pushAndRemoveUntil(
+            MaterialPageRoute(builder: (context) => const HomePage()),
+            (route) => false,
+          );
+        },
+      );
+    } catch (e) {
+      showSnackBar(context, e.toString());
+    }
+  }
 
-//   void login({
-//     required BuildContext context,
-//     required String email,
-//     required String password,
-//   }) async {
-//     try {
-//       var userProvider = Provider.of<UserProvider>(context, listen: false);
-//       final navigator = Navigator.of(context);
-//       http.Response res = await http.post(
-//         Uri.parse('${Constants.uri}/login'),
-//         body: jsonEncode({'email': email, 'password': password}),
-//         headers: <String, String>{
-//           'Content-Type': 'application/json; charset=UTF-8',
-//         },
-//       );
+  void login({
+    required BuildContext context,
+    required String email,
+    required String password,
+  }) async {
+    try {
+      var userProvider = Provider.of<UserProvider>(context, listen: false);
+      final navigator = Navigator.of(context);
+      http.Response res = await http.post(
+        Uri.parse('${Constants.uri}/login'),
+        body: jsonEncode({'email': email, 'password': password}),
+        headers: <String, String>{
+          'Content-Type': 'application/json; charset=UTF-8',
+        },
+      );
 
-//       httpErrorHandle(
-//         response: res,
-//         context: context,
-//         onSuccess: () async {
-//           SharedPreferences prefs = await SharedPreferences.getInstance();
-//           userProvider.setUser(res.body);
-//           await prefs.setString('x-auth-token', jsonDecode(res.body)['token']);
-//           navigator.pushAndRemoveUntil(
-//             MaterialPageRoute(builder: (context) => const Homepage()),
-//             (route) => false,
-//           );
-//         },
-//       );
-//     } catch (e) {
-//       showSnackBar(context, e.toString());
-//     }
-//   }
+      httpErrorHandle(
+        response: res,
+        context: context,
+        onSuccess: () async {
+          SharedPreferences prefs = await SharedPreferences.getInstance();
+          userProvider.setUser(res.body);
+          await prefs.setString('x-auth-token', jsonDecode(res.body)['token']);
+          navigator.pushAndRemoveUntil(
+            MaterialPageRoute(builder: (context) => const HomePage()),
+            (route) => false,
+          );
+        },
+      );
+    } catch (e) {
+      showSnackBar(context, e.toString());
+    }
+  }
 
-//   void signout(BuildContext context) async {
-//     SharedPreferences prefs = await SharedPreferences.getInstance();
-//     prefs.setString('x-auth-token', '');
-//     Navigator.of(context).pushAndRemoveUntil(
-//       MaterialPageRoute(builder: (context) => Login()),
-//       (route) => false,
-//     );
-//   }
-// }
+  void signout(BuildContext context) async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    prefs.setString('x-auth-token', '');
+    Navigator.of(context).pushAndRemoveUntil(
+      MaterialPageRoute(builder: (context) => Login()),
+      (route) => false,
+    );
+  }
+}
