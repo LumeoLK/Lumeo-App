86a1b062352bd1b98cf1274e575d73171a41b09f
Maneth-k <manethkaveen0@gmail.com>
2026-02-07 22:39:26 +0530

    completed basic backend

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
-import authRouter from "./routes/auth.js";
+
 import connectDB from "./config.js";
 import cookieParser from "cookie-parser";
 import cors from "cors";
@@ -10,9 +10,25 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+import authRouter from "./routes/auth.js";
+import sellerRoutes from "./routes/sellerRoutes.js";
+import productRoutes from "./routes/productRoutes.js";
+import customRequestRoutes from "./routes/customreqRoutes.js";
+import cartRoutes from "./routes/cartRoutes.js";
+import orderRoutes from "./routes/orderRoutes.js";
+import reviewRoutes from "./routes/reviewRoutes.js";
+
 app.use(express.json()); //middleware
 app.use(express.static("public"));
-app.use(authRouter);
+
+app.use("/api/auth", authRouter);
+app.use("/api/seller", sellerRoutes);
+app.use("/api/products", productRoutes);
+app.use("/api/requests", customRequestRoutes); 
+app.use("/api/cart", cartRoutes);
+app.use("/api/orders", orderRoutes);
+app.use("/api/reviews", reviewRoutes);
+
 app.use(cookieParser());
 
 try {
