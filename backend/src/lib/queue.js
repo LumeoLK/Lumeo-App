df6130256709325f0d1a1518a5fedae326b01991
MayuraThayalan <thayalanmayura@gmail.com>
2026-03-08 06:57:39 +0530

    added the default redis url to the queue

--- backend/src/lib/queue.js
+++ backend/src/lib/queue.js
@@ -4,10 +4,13 @@ import Redis from "ioredis";
 // 1. Connect to Upstash Redis
 // BullMQ requires maxRetriesPerRequest to be null
 
-const redisConnection = new Redis(process.env.REDIS_URL, {
-  maxRetriesPerRequest: null,
-  enableReadyCheck: false,
-});
+const redisConnection = new Redis(
+  process.env.REDIS_URL || "redis://127.0.0.1:6379",
+  {
+    maxRetriesPerRequest: null,
+    enableReadyCheck: false,
+  },
+);
 
 // 2. Create the "Order Rail" (The Queue)
 // We are naming this queue 'meshy-3d-queue'
