7981515d5ba5eaa5919378cfe00e06a095848678
pulinapasan39 <pulinapasan39@gmail.com>
2026-03-18 05:52:13 +0530

    3rd onboarding page

--- demo/lib/main.dart
+++ demo/lib/main.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 //import 'package:lumeo/pages/user/cart_page.dart';
-import 'package:lumeo/pages/seller/onboarding_page1.dart';
+//import 'package:lumeo/pages/seller/onboarding_page1.dart';
 //import 'package:lumeo/pages/seller/onboarding_page2.dart';
-//import 'package:lumeo/pages/seller/onboarding_page3.dart';
+import 'package:lumeo/pages/seller/onboarding_page3.dart';
 
 void main() {
   runApp(const MyApp());
@@ -18,7 +18,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const SellerOnboardingPage(),
+      home: const SellerOnboardingPage3(),
       debugShowCheckedModeBanner: false,
     );
   }
