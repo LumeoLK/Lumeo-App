c7cbcde68a1fbbb70725021221e723229276547c
pulinapasan39 <pulinapasan39@gmail.com>
2026-03-18 05:49:07 +0530

    created the 3rd last onboarding page of seller side

--- demo/lib/main.dart
+++ demo/lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 //import 'package:lumeo/pages/user/cart_page.dart';
 import 'package:lumeo/pages/seller/onboarding_page1.dart';
 //import 'package:lumeo/pages/seller/onboarding_page2.dart';
+//import 'package:lumeo/pages/seller/onboarding_page3.dart';
 
 void main() {
   runApp(const MyApp());
