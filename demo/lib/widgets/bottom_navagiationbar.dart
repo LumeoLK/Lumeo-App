ff9703850da9ebe094fe1d4a8ddcff2a6e352dd7
pulinapasan39 <pulinapasan39@gmail.com>
2026-03-20 07:33:20 +0530

    added grid functionality and common widgets

--- demo/lib/widgets/bottom_navagiationbar.dart
+++ demo/lib/widgets/bottom_navagiationbar.dart
@@ -18,6 +18,8 @@ class BottomNav extends StatelessWidget {
       backgroundColor: Colors.black,
       selectedItemColor: Colors.white,
       unselectedItemColor: const Color(0xFFE09D3B),
+      type: BottomNavigationBarType.fixed,
+      elevation: 0,
       items: [
         _item('assets/icons/menu.png', 'Home', 0),
         _item('assets/icons/wishlist.png', 'Wish List', 1),
