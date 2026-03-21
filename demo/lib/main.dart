93ef2844814367fd8e4155737a20d52da87fa7a4
Induwara77 <induwaradilshan49@gmail.com>
2026-03-21 16:14:21 +0530

    app bar changes

--- demo/lib/main.dart
+++ demo/lib/main.dart
@@ -1,24 +1,9 @@
 import 'dart:async';
 import 'package:flutter/material.dart';
-// import 'package:lumeo/pages/cart_page.dart';
-import 'pages/wishlist_page.dart';
-import '../pages/seller_dashboard.dart';
-// import '../providers/user_provider.dart';
-import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:get/get.dart';
-// import 'package:lumeo_v2/pages/ar_search_page.dart';
-import 'package:shared_preferences/shared_preferences.dart';
-import 'pages/seller_verification_page.dart';
-// Import your pages
-import '../pages/home_page.dart';
-import '../pages/login.dart';
+import 'pages/home_page.dart';
 import 'pages/onboarding_page1.dart';
-
-// import '../providers/user_provider.dart';
-import "./pages/emptyspace.dart";
-
-// import '../providers/user_provider.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
@@ -34,10 +19,7 @@ class MyApp extends StatelessWidget {
       title: 'Lumeo',
       darkTheme: ThemeData.dark(),
       themeMode: ThemeMode.dark,
-
-      // Senior Tip: Point "home" to SplashScreen so the app flow starts correctly
-      home: const SellerVerificationPage(),
-
+      home: const HomePage(),
       debugShowCheckedModeBanner: false,
     );
   }
