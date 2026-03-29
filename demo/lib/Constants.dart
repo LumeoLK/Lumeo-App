d4d982b0754463b8e882dd0f52a9a964f2dcee7c
MayuraThayalan <thayalanmayura@gmail.com>
2026-03-29 14:12:14 +0530

    changed the ml url

--- demo/lib/Constants.dart
+++ demo/lib/Constants.dart
@@ -8,7 +8,7 @@ class Constants {
   static String authUri = '$baseUrl/api/auth';
 
   // ML backend
-  static const String mlUri = 'https://lumeocs14-lumeo-ml.hf.space';
+  static const String mlUri = 'https://lumeocs14-lumeo-ml-v2.hf.space';
 
   static String cartUri = '$baseUrl/api/cart';
   static String productsUri = '$baseUrl/api/products';
